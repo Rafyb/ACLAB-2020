b2672be9ec7ebf8e082e494d13399ac9be507613
Raphael Bauvin <rbauvin@yahoo.fr>
2020-03-19 08:59:18 +0100

    arbo

--- ICPC-1993/F/exo.py
+++ ICPC-1993/F/exo.py
@@ -1,4 +1,7 @@
-mon_fichier = open("./test.txt", "r")
-print("salut")
+import os
+working_directory = os.getcwd().replace("\\",'/')
+file_path = working_directory + '/ICPC-1993/F/test.txt'
+mon_fichier = open(file_path,"r")
+print(file_path)
 line = mon_fichier.readline()
 mon_fichier.close()
