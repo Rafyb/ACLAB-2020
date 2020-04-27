ff038cedae00cd52b508f006f2322e00eba22ca8
Mjulien-000 <55684769+Mjulien-000@users.noreply.github.com>
2020-04-27 10:58:49 +0200

    Add files via upload

--- ICPC-1995/E/Stamps.py
+++ ICPC-1995/E/Stamps.py
@@ -1,35 +1,32 @@
-def GetLine(file):
+def Ligne(file):
     line = file.readline()
     if not line:
         exit()
     return line.strip()
 
-def CheckData(S, N, L):
-    # Verification du nombre maximal de timbres et du nombre de types de timbres differents
+def Donnee(S, N, L):
     if S > 10 or N > 10:
         return 0
 
-    # Verification de la valeur des timbres
     i = 0
     while i < len(L):
         if int(L[i]) > 100:
             return 0
         i = i + 1
 
-    # Verification du nombre de timbres du jeu donne
     if (len(L) - 1) != int(L[0]):
         return 0
 
     return 1
 
-def GetCombinations(res, stamps):
+def Combinations(res, stamps):
     list = []
     for elem in res:
         for stamp in stamps:
             list.append(elem + ' ' + stamp)
     return list
 
-def IsAPossibleCombination(number, orderedList):
+def PossibleCombination(number, orderedList):
     values = orderedList.split()
     values = [int(i) for i in values]
     if number == sum(values):
@@ -40,27 +37,24 @@ def FindMaxCoverage(maxStamps, setsOfStamps, stamps):
     coverage = 0
     i = 2
     j = 1
-    # On garde uniquement les timbres
     del(stamps[0])
     res = stamps
     allCombinations = []
     allCombinations.append(res)
-    # On enregistre toutes les combinaisons possibles
     while i < maxStamps + 1:
         while j < i:
-            res = GetCombinations(res, stamps)
+            res = Combinations(res, stamps)
             j = j + 1
         allCombinations.append(res)
         i = i + 1
     i = 1
-    # On verifie pour chaque combinaison enregistree jusqu'a qu'un nombre ne soit pas possible a atteindre
     while True:
         possible = False
         j = 0
         while j < len(allCombinations) and possible is False:
             k = 0
             while k < len(allCombinations[j]):
-                if IsAPossibleCombination(i, allCombinations[j][k]) is True:
+                if PossibleCombination(i, allCombinations[j][k]) is True:
                     possible = True
                 k = k + 1
             j = j + 1
@@ -77,15 +71,11 @@ def Stamps(filename):
         case = 1
         max = 0
         while case == 1:
-            # Recuperation du nombre maximum de timbres sur l'enveloppe
-            maxStamps = int(GetLine(file))
-            # Recuperation du nombre de jeux de timbres
-            setsOfStamps = int(GetLine(file))
-            # On recupere chaque jeu de timbre
+            maxStamps = int(Ligne(file))
+            setsOfStamps = int(Ligne(file))
             while setsOfStamps > 0:
-                stamps = GetLine(file).split()
-                # On verifie que les donnees ne sont pas erronnees
-                if CheckData(maxStamps, setsOfStamps, stamps) == 0:
+                stamps = Ligne(file).split()
+                if Donnee(maxStamps, setsOfStamps, stamps) == 0:
                     print('Donnees erronnees')
                     break
                 tmp = FindMaxCoverage(maxStamps, setsOfStamps, stamps)
@@ -96,6 +86,5 @@ def Stamps(filename):
             print"max coverage = " + str(max) + " : ",
             for val in stamps:
                 print val,
-            # On passe au cas suivant
             case = case + 1
 Stamps('test.txt')
