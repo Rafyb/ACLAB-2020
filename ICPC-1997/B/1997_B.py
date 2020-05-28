eb594a26f1f650f713249ff3296141abd0854050
Mjulien-000 <55684769+Mjulien-000@users.noreply.github.com>
2020-05-28 14:48:11 +0200

    Update 1997_B.py

--- ICPC-1997/B/1997_B.py
+++ ICPC-1997/B/1997_B.py
@@ -16,15 +16,24 @@ def main():
             x = int(input('note de la route: '))
             add += x
                 
-##            if add >= rep:
-##                if add > rep or (add == rep and (i - tmp > fin - debut)):
-##                    debut = tmp + 2
-##                    fin = i + 2
+            if add < 0:
+                add = 0
+                tmp = i
+                
+            if add >= rep:
+                if add > rep or (add == rep and (i - tmp > fin - debut)):
+                    debut = tmp + 2
+                    fin = i + 2
                     
-##            rep = add
+            rep = add
             
-##        i += 1
-##        route += 1
+        i += 1
+        route += 1
         
+        if rep > 0:
+            print("The nicest part of route {} is between stops {} and {}".format(route, debut, fin))
+        else:
+            print("Route {} has no nice parts".format(route))
+        test -= 1
 
 main()
